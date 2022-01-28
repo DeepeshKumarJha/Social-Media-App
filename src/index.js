21ab80b53ec13eb5a512dfea7007a8fcbbb68bf6
deepesh <dkj@deepeshs-MacBook-Air.local>
2022-01-28 15:58:01 +0530

    project setup

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider>
       <App />
     </Provider>
   </React.StrictMode>,
